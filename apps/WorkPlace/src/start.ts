445a3b8bfdf389d8903adb3e271568426a3064a5
Mathieuka <mathieu.kahlaoui.pro@gmail.com>
2025-03-09 00:25:29 +0100

    feat(WorkPlace): update start script and add commander dependency
    - Updated start script in package.json to use a single `start` script
    - Added `commander` dependency version `^13.1.0` to devDependencies

---
+++ apps/WorkPlace/src/start.ts
@@ -0,0 +1,32 @@
+import { spawn } from "node:child_process";
+import { Command } from "commander";
+
+const program = new Command();
+
+const validScripts = [
+	"agent_langGraph",
+	"agent_with_custom_tool_langGraph",
+	"sentiment_analysis",
+	"llm_with_chat_model",
+	"semantic_search",
+	"chatbot",
+	"retrieval_augmented_generation",
+];
+
+program.argument("<script>", "The script to run").action((script: string) => {
+	const command = `node --env-file=.env --import=tsx --watch src/${script}.ts`;
+	spawn(command, { stdio: "inherit", shell: true });
+});
+
+if (!validScripts.includes(process.argv.slice(2)[0])) {
+	console.error("Error: Invalid script name provided.\n");
+	console.log("Please provide one of the following valid script names:");
+	for (const script of validScripts) {
+		console.log(`- ${script}`);
+	}
+
+	console.log("\nE.G - npm run start <script-name>");
+	process.exit(1);
+}
+
+program.parse(process.argv);
